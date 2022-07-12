7e7c0c98110973827d2c7d204f48730fe3fb858d
Vytautas <vyckelis@gmail.com>
2022-07-12 15:14:24 +0300

    click count

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 'use strict';
-console.log('main.js');
+// console.log('main.js');
+
+// nusitaikymai
+const btn1El = document.getElementById('btn-click');
+if (!btn1El) throw 'nera btn1El';
+
+let count = 0;
+function showClick() {
+  count++;
+  console.log(`paspausta ${count}`);
+}
+console.log('btn1El ===', btn1El);
+btn1El.addEventListener('click', showClick);
+btn1El.addEventListener('mouseenter', showClick);
+
+// console.log('showClick ===', showClick);
