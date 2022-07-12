ac73dd6e89ab050aca5af8ec9b22c7547a61e7f3
Vytautas <vyckelis@gmail.com>
2022-07-12 16:02:49 +0300

    anonym

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@
 // nusitaikymai
 const btn1El = document.getElementById('btn-click');
 if (!btn1El) throw 'nera btn1El';
+const flexEl = document.getElementById('flex');
+if (!flexEl) throw 'nera flexEl';
+const btnDark = document.getElementById('btn-dark');
+if (!btnDark) throw 'nera btnDark';
+const h1El = document.querySelector('h1');
+if (!h1El) throw 'nera h1Elh1Els';
 
 let count = 0;
 function showClick() {
@@ -15,3 +21,35 @@ btn1El.addEventListener('click', showClick);
 btn1El.addEventListener('mouseenter', showClick);
 
 // console.log('showClick ===', showClick);
+
+// flex dezutei eventas
+
+flexEl.addEventListener('click', flexClick);
+
+// document.body.addEventListener('click', flexClick);
+
+function flexClick(event) {
+  const elAntKurioPaspaudziau = event.target;
+  console.log(elAntKurioPaspaudziau);
+  if (elAntKurioPaspaudziau.className === 'card') {
+    console.log('card');
+    elAntKurioPaspaudziau.style.backgroundColor = 'tomato';
+    elAntKurioPaspaudziau.textContent += ' sp';
+  }
+}
+
+// h1TitleChange();
+// console.log('blue ===', blue);
+// btnDark.addEventListener('click', h1TitleChange);
+// anonimine funkcija function () {}
+btnDark.addEventListener('click', function () {
+  h1El.style.backgroundColor = 'tomato';
+});
+
+// function hoisting - tik kai yra function declaration
+// funkcijos pernesamos i failo virsu
+// function h1TitleChange() {
+//   h1El.style.backgroundColor = 'tomato';
+// }
+
+// const blue = 45;
